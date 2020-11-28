8e99b71426165d53a0f951fdb1ef213376b6915c
mohehexd <dumblemoe@gmail.com>
2020-11-27 21:44:44 -0600

    Creating card array

---
+++ app.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options-
+    const cardArray = [
+        {
+            name: 'borb',
+            img: 'images/borb.png'
+        },
+        {
+            name: 'borb',
+            img: 'images/borb.png'
+        },
+        {
+            name: 'fishbowl',
+            img: 'images/fishbowl.png'
+        },
+        {
+            name: 'fishbowl',
+            img: 'images/fishbowl.png'
+        },
+        {
+            name: 'food',
+            img: 'images/food.png'
+        },
+        {
+            name: 'food',
+            img: 'images/food.png'
+        },        
+        {
+            name: 'french_hat',
+            img: 'images/french_hat.png'
+        },
+        {
+            name: 'french_hat',
+            img: 'images/french_hat.png'
+        },
+        {
+            name: 'garfield',
+            img: 'images/garfield.png'
+        },
+        {
+            name: 'garfield',
+            img: 'images/garfield.png'
+        },
+        {
+            name: 'llama',
+            img: 'images/llama.png'
+        },        
+        {
+            name: 'llama',
+            img: 'images/llama.png'
+        },
+        {
+            name: 'panda',
+            img: 'images/panda.png'
+        },
+        {
+            name: 'panda',
+            img: 'images/panda.png'
+        },
+        {
+            name: 'snail',
+            img: 'images/snail.png'
+        },        
+        {
+            name: 'snail',
+            img: 'images/snail.png'
+        }
+    ]
+})
